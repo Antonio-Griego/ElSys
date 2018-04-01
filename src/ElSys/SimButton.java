8a86aec3ea4550403cb891219693201c0d23f120
Antonio Griego <deano505@unm.edu>
2018-03-31 20:31:58 -0600

    implemented SimButton and SimPhysLocation
    The SimButton and SimPhysLocation classes have been updated
    and implemented to follow the program specs defined in the SAD.

--- src/ElSys/SimButton.java
+++ src/ElSys/SimButton.java
@@ -1,36 +1,70 @@
 package ElSys;
 
 import ElSys.Enums.ButtonLight;
+import java.util.Random;
 
+/**
+ * This class represents a simulated button in an elevator cabin.
+ *
+ * @author Antonio Griego
+ */
 public class SimButton
 {
-  private boolean isPressed = false;
-  private ButtonLight buttonLight = ButtonLight.OFF;
+  private boolean isPressed;
+  private ButtonLight buttonLight;
+  private Random random;
 
-  public void setLight(ButtonLight buttonLight)
+  /**
+   * Constructor method.
+   *
+   * @param random Random number generator object.
+   */
+  SimButton(Random random)
   {
-    this.buttonLight = buttonLight;
+    this.isPressed = false;
+    this.buttonLight = ButtonLight.OFF;
+    this.random = random;
   }
 
-  public ButtonLight getLight()
+  /**
+   * This method sets the status of the button light: true = on, false = off.
+   * The status of the light is printed out to the console and the button
+   * press status is updated: light on = button is pressed, light off = button
+   * is not pressed.
+   *
+   * @param isButtonLightOn
+   */
+  public void setLight(boolean isButtonLightOn)
   {
-    return buttonLight;
+    if(isButtonLightOn)
+    {
+      System.out.println("ButtonLight = ON");
+      this.buttonLight = ButtonLight.ON;
+      isPressed = true;
+    }
+    else
+    {
+      System.out.println("ButtonLight = OFF");
+      this.buttonLight = ButtonLight.OFF;
+      isPressed = false;
+    }
   }
 
+  /**
+   * This method returns the pressed status of the button: true = pressed,
+   * false = not pressed. If the button is NOT pressed, there is a 10% chance
+   * that the button will change state and become pressed.
+   *
+   * @return True = button is pressed; False = button is not pressed
+   */
   public boolean isPressed()
   {
+    // change state if and only if isPressed = false
+    if(!isPressed)
+    {
+      isPressed = (random.nextDouble() <= 0.1);
+    }
+
     return isPressed;
-  }
-
-  public void press()
-  {
-    setLight(ButtonLight.ON);
-    isPressed = true;
-  }
-
-  public void reset()
-  {
-    buttonLight = ButtonLight.OFF;
-    isPressed = false;
   }
 }
--- src/ElSys/SimPhysLocation.java
+++ src/ElSys/SimPhysLocation.java
@@ -1,22 +1,65 @@
 package ElSys;
 
+/**
+ * This class stores the simulated location of an elevator.
+ *
+ * @author Antonio Griego
+ */
 public class SimPhysLocation
 {
-  private int currentHeightInFeet = -1;
-  private int currentFloor = -1;
+  private final int floors;
+  private double location; // unit of distance = floors
 
-  public void move(int newFloor)
+  /**
+   * Constructor method.
+   *
+   * @param floors The maximum number of floors in the building.
+   */
+  SimPhysLocation(int floors)
   {
-    currentFloor = newFloor;
+    this.floors = floors;
+    this.location = 0.0;
   }
 
-  public int getPhysicalLocation()
+  /**
+   * Move the elevator the specified distance in floor units. For example,
+   * move(0.5) moves the elevator up 1/2 of a floor and move(-2.0) moves the
+   * elevator down two floors.
+   *
+   * If the distance moved would result in the location being out of bounds,
+   * the value is truncated so that it simply moves the elevator the maximum
+   * distance allowed. For example, when on the ground floor, move(1337.0)
+   * will move the elevator to the top floor (the 10th floor) and location
+   * will equal 9.0.
+   *
+   * @param distance The distance in floor units to move the elevator.
+   */
+  public void move(double distance)
   {
-    return currentHeightInFeet;
+    if(location + distance < 0)
+    {
+      location = 0.0;
+    }
+    else if((location + distance) > (floors - 1))
+    {
+      location = floors - 1;
+    }
+    else
+    {
+      location += distance;
+    }
   }
 
-  public int getCurrentFloor()
+  /**
+   * This method returns the physical location of the elevator in floor units,
+   * i.e., a value of 0.0 indicates the elevator is on floor 1 and 5.5
+   * indicates the elevator is on floor 6.5 (halfway between floor 6 and 7).
+   *
+   * @return The physical location of the elevator in floor units in the range
+   *         of [0.0, (N-1)] (for an N story building).
+   */
+  public double getPhysicalLocation()
   {
-    return currentFloor;
+    return location;
   }
 }
