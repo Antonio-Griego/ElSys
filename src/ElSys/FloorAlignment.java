cb8a70485ddb014d080824a2b86da4bf18a6e03f
Mike G <glennon@unm.edu>
2018-04-08 14:45:35 -0600

    FloorAlignment updated: creates a thread to constantly check if aligned with a floor, new methods added, old ones removed. Component spec has been edited to reflect changes.

--- src/ElSys/FloorAlignment.java
+++ src/ElSys/FloorAlignment.java
@@ -1,31 +1,66 @@
 package ElSys;
 
+import java.lang.*;
+
 public class FloorAlignment
 {
   private SimPhysLocation simPhysLocation;
   private int floor;
+  private worker w;
 
   FloorAlignment(SimPhysLocation simPhysLocation)
   {
     this.simPhysLocation = simPhysLocation;
+    w = new worker(simPhysLocation);
   }
 
+  // when called, grabs the most current floor from worker thread and returns it
   public int getCurrentFloor()
   {
-    // makes the double to int cast with the appropriate rounding.
-    floor = (int) (simPhysLocation.getPhysicalLocation()+.5);
+    floor = w.floor;
     return floor;
   }
 
-  public double getDistanceToAlign()
+  public boolean isAligned()
   {
-    floor = getCurrentFloor();
-    return (floor - simPhysLocation.getPhysicalLocation());
-    // returns positive and negative values, not absolute value of distance
+    if (simPhysLocation.getAlignedFloor() == -1){return false;}
+    else {return true;}
   }
 
   public boolean reachedEndOfShaft()
   {
     return simPhysLocation.reachedEndOfShaft();
+  }
+}
+
+// worker thread to constantly check if alligned with a floor
+// since only this thread sets floor, there should be no concurrency issues.
+class worker extends Thread
+{
+  private SimPhysLocation simPhysLocation;
+  public int floor;
+
+  worker(SimPhysLocation simPhysLocation)
+  {
+    this.simPhysLocation = simPhysLocation;
+    start();
+  }
+
+  public void run()
+  {
+    while (true)
+    {
+      work();
+    }
+  }
+
+  public void work()
+  {
+    int location = simPhysLocation.getAlignedFloor();
+    if (location == -1){}
+    else
+    {
+      floor = location;
+    }
   }
 }
