4eed27f2fedb18a2cebc10dab2c8befdd55ba243
Mike G <glennon@unm.edu>
2018-04-08 15:02:13 -0600

    MotorControl updated: moveElevator now sends movement over the course of a second. There are 60 calls to simLocation now over a second of time each sending 1/60th of the movement distance. Component spec reflected to changes.

--- src/ElSys/MotorControl.java
+++ src/ElSys/MotorControl.java
@@ -11,6 +11,13 @@ public class MotorControl
 
   void moveElevator(double distance)
   {
-    simPhysLocation.move(distance);
+    double step = distance/60;
+    double t = 1000/60;
+
+    for (int i=0; i<60; i++)
+    {
+      simPhysLocation.move(step);
+      sleep(t);
+    }
   }
 }
